44c7c0e32ea559e2f115bec26e095fcb4fbc03b0
Pancho157 <hoxocraft65@gmail.com>
2022-04-05 20:38:40 -0300

    Generadas la página de contacto y la de error 404

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,60 @@
 // Esta página está destinada a poseer un formulario para reportar errores y posibles mejoras
-
-import "./inProgress.css"
+// Cuando esté totalmente implementado solo podrán enviár reportes los que tengan iniciada su sessión
+import { useState } from "react";
+import "./styles/Contact.css";
 function Contact() {
-    return (
-      <>
-          <h1 className="inProgress">Esta página se encuentra en desarrollo</h1>
-      </>
-    );
-  }
-  
-  export default Contact;
+  const [asunto, setAsunto] = useState("Asunto");
+  const [mensaje, setMensaje] = useState("Mensaje");
+
+  const cambiarAsunto = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setAsunto(value);
+  };
+
+  const cambiarMensaje = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setMensaje(value);
+  };
+
+  return (
+    <>
+      {/* <h1 className="inProgress">Esta página se encuentra en desarrollo</h1> */}
+
+      <main className="contact--main">
+        <section>
+          <form className="contact--form_container">
+            <h1 className="contact--form_title">
+              Cuentanos sobre lo que viste!
+            </h1>
+            <p>Siempre se agradece la información y participación que nos pueda brindar</p>
+            <div>
+              <input
+                className="contact--form_input"
+                id="asunto"
+                name="Asunto"
+                value={asunto}
+                onChange={cambiarAsunto}
+              ></input>
+
+              <textarea
+                className="contact--form_message"
+                id="mensaje"
+                name="mensaje"
+                value={mensaje}
+                onChange={cambiarMensaje}
+              ></textarea>
+            </div>
+
+            <button className="contact--form_button" onClick={cambiarMensaje}>
+              Enviar
+            </button>
+          </form>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Contact;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 // El destino de la presente página es el de mostrar al usuario cuando hay un error en la URL
 
-import "./inProgress.css"
+import "./styles/NotFound.css";
 
 function NotFound() {
-    return (
-      <>
-          <h1 className="inProgress">Esta página se encuentra en desarrollo</h1>
-      </>
-    );
-  }
-  
-  export default NotFound;
+  return (
+    <>
+      <div className="NotFound--container">
+        <h1 className="NotFound--title">Lo sentimos</h1>
+        <p>
+          No se encontró la página que está buscando :(
+        </p>
+        <p>Error: <span className="NotFound--error">404</span></p>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
