3315ac2c8362072ffe2f5eed59d1273c88e10e38
Pancho157 <hoxocraft65@gmail.com>
2022-04-25 21:46:03 -0300

    Generado el loader para la carga de los productos

--- src/components/Main/ItemList.js
+++ src/components/Main/ItemList.js
@@ -1,7 +1,7 @@
 // Este archivo simula una llamada a una API, recorre los productos (array de objetos) y pasa la información al componente <Item />, el cual los renderiza
 // También actua como un contenedor de segundo nivel de las Cards (El de primer nivel es el <main></main>)
 
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 // Components
@@ -11,14 +11,11 @@ import Item from "./Item";
 // Styles
 import "./styles/ItemList.css";
 
-// Context
-import CartContext from "../../context/CartContext";
-
 const ItemList = () => {
   const navigate = useNavigate();
-  const { cartProducts, addProductToCart } = useContext(CartContext);
   const { category } = useParams();
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getProducts = new Promise((resolve, reject) => {
     // Trae los productos desde el archivo (data.js) mediante una promesa que se resuelve luego de 2 segundos
@@ -32,6 +29,7 @@ const ItemList = () => {
     try {
       const result = await getProducts;
       setProducts([]);
+      setLoading(false);
       // Verifica que se haya pasado un valor por useParams (en category)
       category !== undefined
         ? filterProductsByCategory(mainPageProducts, category)
@@ -51,6 +49,8 @@ const ItemList = () => {
   };
 
   useEffect(() => {
+    setLoading(true);
+    setProducts([]);
     getApiProducts();
   }, [category]);
 
@@ -62,30 +62,44 @@ const ItemList = () => {
   // Llama a la función que almacena los datos en el array products, los recorre con el map y los renderiza con el <Item {props} />
   return (
     <section className="cards-container">
-      {/* Recorre el resultado de array luego de pasar por el filtro (si es que pasa) y lo renderiza */}
-      {products.map((product) => {
-        return (
-          <div
-            key={product.id}
-            onClick={() => {
-              changeToDetailPage(product.category, product.id);
-            }}
-          >
-            {/* Cuando se haga click sobre el producto va a llamar a la función que muestra el detalle del mismo */}
-            <Item
-              id={product.id}
-              imgUrl={product.imgUrl}
-              title={product.title}
-              brand={product.brand}
-              model={product.model}
-              price={product.price}
-              stock={product.stock}
-              description={product.description}
-              quantity={product.quantity}
-            />
-          </div>
-        );
-      })}
+      {loading ? (
+        <>
+          <h2>Cargando...</h2>
+          <img
+            className="loader"
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSULkw68WhWUaUu0yvFxWl9KfocVcEa00VPGg&usqp=CAU"
+            }
+            alt={"Loader"}
+          />
+        </>
+      ) : (
+        <>
+          {products.map((product) => {
+            return (
+              <div
+                key={product.id}
+                onClick={() => {
+                  changeToDetailPage(product.category, product.id);
+                }}
+              >
+                {/* Cuando se haga click sobre el producto va a llamar a la función que muestra el detalle del mismo */}
+                <Item
+                  id={product.id}
+                  imgUrl={product.imgUrl}
+                  title={product.title}
+                  brand={product.brand}
+                  model={product.model}
+                  price={product.price}
+                  stock={product.stock}
+                  description={product.description}
+                  quantity={product.quantity}
+                />
+              </div>
+            );
+          })}
+        </>
+      )}
     </section>
   );
 };
