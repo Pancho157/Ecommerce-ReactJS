a1632dfaca0a032bcbc78f50714fdfae7d306fec
Pancho157 <hoxocraft65@gmail.com>
2022-05-18 22:04:38 -0300

    Generados los estilos de la página de ofertas

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -3,10 +3,13 @@ import { useEffect, useState } from "react";
 import database from "../data/firebase";
 import { useNavigate } from "react-router-dom";
 import Item from "../components/Main/Item";
+import "./styles/Offers.css";
 
 function Offers() {
   const navigate = useNavigate();
   const [offerProducts, setOfferProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const getProducts = async () => {
     const itemsCollection = collection(database, "productos");
     const productosSnapshot = await getDocs(itemsCollection);
@@ -30,38 +33,58 @@ function Offers() {
   };
 
   useEffect(() => {
+    setLoading(true);
     setOfferProducts([]);
 
     getProducts().then((productos) => {
+      setLoading(false);
       filterOfferProducts(productos);
     });
   }, []);
 
   return (
-    <>
-      {offerProducts.map((product) => {
-        return (
-          <div
-            key={product.id}
-            onClick={() => {
-              changeToDetailPage(product.category, product.id);
-            }}
-          >
-            <Item
-              id={product.id}
-              imgUrl={product.imgUrl}
-              title={product.title}
-              brand={product.brand}
-              model={product.model}
-              price={product.price}
-              stock={product.stock}
-              description={product.description}
-              quantity={product.quantity}
+    <main className="offers-main">
+      <h2 className="main-tittle">Productos de oferta</h2>
+      <section className="cards-container">
+        {loading ? (
+          <>
+            <h2>Cargando...</h2>
+            <img
+              className="loader"
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSULkw68WhWUaUu0yvFxWl9KfocVcEa00VPGg&usqp=CAU"
+              }
+              alt={"Loader"}
             />
-          </div>
-        );
-      })}
-    </>
+          </>
+        ) : (
+          <>
+            {offerProducts.map((product) => {
+              return (
+                <div
+                  key={product.id}
+                  onClick={() => {
+                    changeToDetailPage(product.category, product.id);
+                  }}
+                >
+                  <Item
+                    id={product.id}
+                    imgUrl={product.imgUrl}
+                    title={product.title}
+                    brand={product.brand}
+                    model={product.model}
+                    price={product.price}
+                    stock={product.stock}
+                    description={product.description}
+                    quantity={product.quantity}
+                  />
+                </div>
+              );
+            })}
+          </>
+        )}
+      </section>
+    </main>
   );
 }
 
